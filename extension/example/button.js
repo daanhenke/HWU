d6761b066996db68cbe2e9f2ca2b239669b829f1
daanhenke <daanhenkers@gmail.com>
2016-01-14 18:44:54 +0100

    Create button.js

---
+++ extension/example/button.js
@@ -0,0 +1,5 @@
+var button = document.createElement('button');
+var button_text = document.createTextNode('I am awesome');
+button.appendChild(button_text);
+document.body.appendChild(button);
+//adds a button the the dom
